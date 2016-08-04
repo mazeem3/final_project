4acd7ec0116b81fb8c26ed9787161d0f82e5e446
mazeem3 <maaz.azeem3@yahoo.com>
2016-08-04 10:21:49 -0500

    Added travis

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,18 +2,6 @@ class MapsController < ApplicationController
   def index
   end
 
-  def results
-  	@food = params[:food]
-  	@location = params[:location]
-  	parameters = { term: @food, limit: 9 }
 
-  	@results = Yelp.client.search(@location, parameters)
-  end
-
-  def test
-    parameters = { term: "pancakes", limit: 10 }
-
-    @results = Yelp.client.search('Atlanta, GA', parameters)
-  end
 
 end
